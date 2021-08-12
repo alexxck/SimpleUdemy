472c26468f24af93b1bcc836e9b5e67a230dc94b
alexxck <boguslavets@gmail.com>
2021-08-12 14:35:57 +0300

    stars_number & cover

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 class Course < ApplicationRecord
+  def star_number
+    self.star.blank? ? 1 : self.star
+  end
+  def cover
+    self.image.blank? ? "default.png" : self.image
+  end
+
+  # if star.blank?
+  #  1
+  # else
+  #  star
+  # end
 end
